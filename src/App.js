1a8c18481df82fc3b9450d5344595ed15915c474
maksymlivshyn <maksym.livshyn@northerndata.de>
2022-10-06 15:44:28 +0200

    change name of preview environment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is staging environment
+          This is staging environment!
         </a>
       </header>
     </div>
